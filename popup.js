7c8a99a943d64891b71a4e846cb094bb4b5244a6
Tristan Blond <68480964+Tantris514@users.noreply.github.com>
2024-01-07 13:55:43 -0500

    Added storage and html preset

--- popup.js
+++ popup.js
@@ -1,3 +1,19 @@
+// This function updates the select element to reflect the current theme
+function setCurrentThemeSelection(currentTheme) {
+    var themeSelector = document.getElementById('themeSelector');
+    themeSelector.value = currentTheme;
+}
+
+// Get the currently selected theme from storage and update the select element
+chrome.storage.sync.get('selectedTheme', function(data) {
+    if (data.selectedTheme) {
+        setCurrentThemeSelection(data.selectedTheme);
+    } else {
+        // If there's no theme saved, set it to the default value
+        setCurrentThemeSelection('default');
+    }
+});
+
 document.getElementById('applyButton').addEventListener('click', function() {
     var selectedTheme = document.getElementById('themeSelector').value;
 
